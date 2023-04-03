14a6c42b548c499ea20a2b127c2ff5e793c25868
illmito <thomas.brayovic@gmail.com>
2023-04-03 19:24:49 +1000

    3/4/2023 - Cogs added.

--- bot.py
+++ bot.py
@@ -28,50 +28,10 @@ async def main():
 
 asyncio.run(main())
 
-# @client.command()
-# async def ping(ctx):
-#     bot_latency = round(client.latency * 1000)
-#
-#     await ctx.send(f"{bot_latency} ms")
-#
-#
+
 # # random 5 name generator for a match in csgo
 # @client.command(aliases=["csgo", "teamroll", "wheelofnames"])
 # async def wheel_of_names(ctx, *args):
 #     players = args
 #     chosen_players = random.sample(players, 5)
 #     await ctx.send(f"**The players are...**  \n> `{', '.join(chosen_players)}`")
-#
-#
-# # 8ball magic for some fun
-# @client.command(aliases=["8ball", "eightball"])
-# async def magic_eightball(ctx, *, question):
-#     with open("responses.txt", "r") as f:
-#         random_responses = f.readlines()
-#         response = random.choice(random_responses)
-#
-#     await ctx.send(response)
-#
-#
-# # quote recording
-# @client.command(aliases=["nickquote", "nicksaid"])
-# async def nick_quote(ctx, *, args):
-#     with open("quotes.txt", "a") as n:
-#         quotes = str(args)
-#         n.write(f"\n{quotes}")
-#         n.close()
-#     await ctx.send(f"`{quotes}`, has been added to the quotes.")
-#
-#
-# @client.command(aliases=["whatnicksay", "nicksays"])
-# async def nick_says(ctx):
-#     with open("quotes.txt", "r") as f:
-#         random_quotes = f.readlines()
-#         quotes = random.choice(random_quotes)
-#     await ctx.send(quotes)
-#
-#
-# # async def load():
-# #     for filename in os.listdir("./cogs"):
-#
-# client.run(TOKEN)
